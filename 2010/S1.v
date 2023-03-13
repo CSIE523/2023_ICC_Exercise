ca3b1ce708eb61cf0e92a7e4eb6c3cc2a074a1f4
CSIE523 <s99999915@gmail.com>
2023-03-14 00:14:35 +0800

    2010 Finished

---
+++ 2010/S1.v
@@ -0,0 +1,103 @@
+module S1(clk,
+	  rst,
+	  RB1_RW,
+	  RB1_A,
+	  RB1_D,
+	  RB1_Q,
+	  sen,
+	  sd);
+
+input clk, rst;
+output reg RB1_RW;      // control signal for RB1: Read/Write
+output  [4:0] RB1_A; // control signal for RB1: address
+output reg [7:0] RB1_D; // data path for RB1: input port
+input [7:0] RB1_Q;  // data path for RB1: output port
+output reg sen, sd;
+
+
+reg [1:0]state, next_state;
+parameter IDLE = 2'd0,
+	  READ = 2'd1,
+	  OUT = 2'd2;
+
+reg r_w;
+reg [2:0]addr;
+reg [17:0]data;
+reg [4:0]counter;
+
+assign RB1_A = counter;
+
+always@(posedge clk or posedge rst)begin
+    if(rst)
+        state <= IDLE;
+    else 
+        state <= next_state;
+end
+
+always@(*)begin
+    if(rst)
+        next_state = IDLE;
+    else begin
+        case(state)
+            IDLE:
+                next_state = READ;
+            READ:begin
+                if(counter == 19) next_state = OUT;
+                else next_state = READ;  
+            end
+            OUT:begin
+				if(counter == 18) next_state = IDLE;
+                else next_state = OUT; 
+			end
+			default:    next_state = IDLE;
+        endcase
+    end 
+end
+
+
+//DATA INPUT
+always@(posedge clk or posedge rst)begin
+    if(rst)begin
+		RB1_RW <= 1;
+		sen <= 1;
+		RB1_D <= 0;
+		sd <= 0;
+		r_w <= 0;
+		addr <= 0;
+		counter <= 0;
+    end
+    else begin
+		if(next_state == IDLE)begin
+			counter <= 0;
+			sen <= 1;
+			addr <= addr + 1;
+		end
+		else if(next_state == READ)begin
+			data[counter-1] <= RB1_Q[7-addr];
+			counter <= counter + 1;
+		end
+		else if(next_state == OUT)begin
+			case(counter)
+				19:begin
+					sen <= 0;
+					sd <= addr[2];
+					counter <= 30;
+				end
+				30:begin
+					sd <= addr[1];
+					counter <= counter + 1;
+				end
+				31:begin
+					sd <= addr[0];
+					counter <= counter + 1;
+				end
+				default:begin
+					sd <= data[17-counter];
+					counter <= counter + 1;
+				end
+			endcase
+		end
+    end
+end
+
+endmodule
---
+++ 2010/S2.v
@@ -0,0 +1,83 @@
+module S2(clk,
+	  rst,
+	  S2_done,
+	  RB2_RW,
+	  RB2_A,
+	  RB2_D,
+	  RB2_Q,
+	  sen,
+	  sd);
+
+input clk, rst;
+output S2_done;
+output reg RB2_RW;
+output reg [2:0] RB2_A;
+output reg [17:0] RB2_D;
+input [17:0] RB2_Q;
+input sen, sd;
+
+reg [2:0]state, next_state;
+parameter IDLE = 3'd0,
+	  DELAY = 3'd1,
+	  READ = 3'd2,
+	  OUT = 3'd3,
+	  FINISH = 3'd4;
+
+reg [4:0] counter_RB2;
+
+assign S2_done = (state == FINISH);
+
+always@(posedge clk or posedge rst)begin
+    if(rst)
+        state <= IDLE;
+    else 
+        state <= next_state;
+end
+
+always@(*)begin
+    if(rst)
+        next_state = IDLE;
+    else begin
+        case(state)
+            IDLE:
+                if(sen == 0) next_state = READ;
+				else next_state = IDLE;
+            READ:begin
+                if(counter_RB2 == 21) next_state = OUT;
+                else next_state = READ;  
+            end
+            OUT:begin
+				if(RB2_A == 7) next_state = FINISH;
+                else next_state = IDLE; 
+			end
+            default:    next_state = IDLE;
+        endcase
+    end 
+end
+
+
+//DATA INPUT
+always@(posedge clk or posedge rst)begin
+    if(rst)begin
+		RB2_RW <= 1;
+		RB2_A <= 0;
+		RB2_D <= 0;
+		counter_RB2 <= 0;
+	end
+    else begin
+ 		if(next_state == READ)begin
+			if(counter_RB2 <= 2)
+				RB2_A[2-counter_RB2] <= sd; 
+			else 
+				RB2_D[20-counter_RB2] <= sd;
+			counter_RB2 <= counter_RB2 + 1;
+		end
+		else if(next_state == OUT)begin
+			RB2_RW <= 0;
+			counter_RB2 <= 0;
+		end
+
+    end
+end
+
+endmodule
